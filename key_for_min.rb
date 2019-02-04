b904558392c824b17fd3ba31b1f796af651d4eb2
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 01:31:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest = 0
   name_hash.each do |key, value|
-    if value.nil? || value < lowest
+    if value == 0 || value < lowest
       lowest = value
     end
   end
