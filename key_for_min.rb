5d8bed91498eb1a9d9b7adfdb896def4f6cfdb2a
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 01:27:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |key, value|
-    key.min
+  lowest = nil
+  name_hash.each do |key, value|
+    if value.nil? || value < lowest
+      lowest = value
+    end      
   end
 end
